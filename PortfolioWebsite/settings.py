c2f7565825130ac8fc5b5458a5507034c42d580b
r_narula <aarav30302001@gmail.com>
2020-11-06 01:54:18 +0530

    Raghav

--- PortfolioWebsite/settings.py
+++ PortfolioWebsite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'z#*-5r@og2^w3g8zu+cm$&e&n=keb*l^^b@5r@qg9$o0qw34)*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['rnarula.herokuapp.com']
 
@@ -125,3 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+
+
+django_heroku.settings(locals())
